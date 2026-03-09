183ede10a7310416ae765ec35b151f4c87d38450
sourjo-ghosh <joyghosh211@outlook.com>
2026-03-10 05:39:47 +0600

    Login ui and logic section added

---
+++ script.js
@@ -0,0 +1,17 @@
+const userNameInp = document.getElementById("username-input")
+const userPasswordInp = document.getElementById("userpassword-input")
+const signUpBtn = document.getElementById("signup-btn")
+
+signUpBtn.addEventListener('click', () => {
+    const userNameVal = userNameInp.value.trim();
+    const userPasswordVal = userPasswordInp.value.trim();
+    if(userNameVal === "admin".trim() && userPasswordVal === "admin123".trim()){
+        document.getElementById("incorrect-password").classList.add("hidden")
+        userNameInp.value = "";
+        userPasswordInp.value = "";
+    } else{
+        document.getElementById("incorrect-password").classList.remove("hidden");
+        userNameInp.value = "";
+        userPasswordInp.value = "";
+    }
+})
